333ad32ecbc64dbe22a5ef25599a5aa44cb3c1f9
Lilian Baxan <lilian.baxan@gmail.com>
2022-12-01 13:41:27 +0000

    worked + design

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-from segment import process_PSP_base, process_polyacov
+from segment import process_PSP_base, process_Gray_PSP
 
 st.title("EYE'S segmentation demo")
 results = []
@@ -9,29 +9,32 @@ image_file = st.file_uploader(
     'Load an eye image (PNG or JPG)', type=['png', 'jpg'])
 
 # Выполнение блока, если загружено изображение
-results_polyakov = None
+results_grey_psp = None
 if not image_file is None:                    # Выполнение блока, если загружено изображение
     # Создание 2 колонок # st.beta_columns(2)
     col1, col2, col3 = st.columns(3)
-    col2.header('Col-2')
+    col2.header('   -')
+    col2.text('SEGMENTATION result')
     # col2.text(f'st.image type={type(image_file)}')
     image = Image.open(image_file)            # Открытие изображения
     # Обработка изображения с помощью функции, реализованной в другом файле
     results = process_PSP_base(image_file)
-    results_polyakov = process_polyacov(image_file, col2)
+    results_grey_psp = process_Gray_PSP(image_file, col2)
+
     with col1:
         st.header('Original image')
         st.text('PSP Color-256')
         # Вывод в первой колонке уменьшенного исходного изображения
         st.image(results[0])
         st.text(
-            'SEGMENTATION result\nFinal_Baxan_PSP_std_color_256x256_Real_Augment')
+            'Final_Baxan_PSP_std_color_256x256_Real_Augment')
         st.image(results[1])                 # Вывод маски второй колонке
 
     with col3:
-        st.header('U-NET')
-        st.text(' --- ')
-        if not results_polyakov[0] == None:
-            st.image(results_polyakov[0])
-        st.text('Grayscale case - Model')
-        # st.image(results_polyakov[1])
+        st.header(' -')
+        st.text('PSP-Grey-256')
+        if not results_grey_psp[0] == None:
+            st.image(results_grey_psp[0])
+        st.text('MODEL_modelPSP_REAL72_grayscale.h5 ')
+        # st.text(f'type {results_grey_psp[1]} {len(results_grey_psp[1])}')
+        st.image(results_grey_psp[1])
--- segment.py
+++ segment.py
@@ -3,9 +3,11 @@ from PIL import Image, ImageOps
 from keras.models import load_model
 from keras.utils import img_to_array, load_img
 
-MODEL_NAME_polyakov = 'polyakov_unet5_1e3.h5'
+MODEL_NAME_Gray_PSP_Baxan = 'MODEL_modelPSP_REAL72_grayscale.h5'
 MODEL_NAME = 'MODEL_WEIGHTS_Final_Baxan_PSP_std_color_256x256_Real_Augment.h5'
 model = load_model(MODEL_NAME)  # Загружаем веса
+model_grey = load_model(MODEL_NAME_Gray_PSP_Baxan)  # Загружаем веса
+
 IMG_WIDTH = 256
 IMG_HEIGHT = 256
 INPUT_SHAPE = (IMG_WIDTH, IMG_HEIGHT, 1)
@@ -17,7 +19,7 @@ Sklera = (0, 0, 255)              # Склера (синий)
 Other = (0, 0, 0)          # Окружение глаза ? ()
 
 # список меток классов:
-CLASS_LABELS = (Other, Zracek, Radujka, Sklera)
+CLASS_LABELS_Col = (Other, Zracek, Radujka, Sklera)
 # Функция преобразования тензора меток класса в цветное сегметрированное изображение
 
 
@@ -29,9 +31,33 @@ def labels_to_rgb(image_list  # список одноканальных изоб
         # Создание пустой цветной картики
         temp = np.zeros((IMG_WIDTH, IMG_HEIGHT, 3), dtype='uint8')
         # По всем классам:
-        for i, cl in enumerate(CLASS_LABELS):
+        for i, cl in enumerate(CLASS_LABELS_Col):
             # Нахождение пикселов класса и заполнение цветом из CLASS_LABELS[i]
-            temp[np.where(np.all(y == i, axis=-1))] = CLASS_LABELS[i]
+            temp[np.where(np.all(y == i, axis=-1))] = CLASS_LABELS_Col[i]
+        result.append(temp)
+    return np.array(result)
+
+
+Zracek_G = 76         # Зрачек (красный)
+Radujka_G = 150           # Радужка (зеленый)
+Sklera_G = 29              # Склера (синий)
+Other_G = 0          # Окружение глаза ? ()
+# список меток классов:
+CLASS_LABELS_G = (Other_G, Zracek_G, Radujka_G, Sklera_G)
+# Функция преобразования тензора меток класса в сегметрированное изображение
+
+
+def labels_to_grayscale(image_list  # список одноканальных изображений
+                        ):
+    result = []
+    # Для всех картинок в списке:
+    for y in image_list:
+        # Создание пустой GRAYSCALE  картики
+        temp = np.zeros((IMG_WIDTH, IMG_HEIGHT, 1), dtype='uint8')
+        # По всем классам:
+        for i, cl in enumerate(CLASS_LABELS_G):
+            # Нахождение пикселов класса и заполнение цветом из CLASS_LABELS[i]
+            temp[np.where(np.all(y == i, axis=-1))] = CLASS_LABELS_G[i]
         result.append(temp)
     return np.array(result)
 
@@ -51,7 +77,7 @@ def process_PSP_base(image_file):
     return resized_image, prediction_array,
 
 
-def process_polyacov(image_file, col):
+def process_Gray_PSP(image_file, col):
     prediction_array_grey = None
     # image = type=<class 'PIL.PngImagePlugin.PngImageFile'>
     image = Image.open(image_file)
@@ -63,18 +89,21 @@ def process_polyacov(image_file, col):
     # Загружаем изображение для функции предсказания.
     # img0 = image.load_img(im, color_mode='grayscale', target_size=(IMG_HEIGHT, IMG_WIDTH))
 
-    # image_grX shape = (256, 256, 1)
+    # image_gr.shape = (256, 256, 1)
+    aa = []
     image_grX = img_to_array(image_gr)
-    # col.text(f'image_grX shape = {image_grX.shape}')
-    # image_grX = np.squeeze(image_grX, axis=-1)
+    aa.append(image_grX)
+    aa = np.array(aa)
+    # col.text(f'aa shape = {aa.shape}')
     # col.text(f'image_grX_2 shape = {image_grX.shape}')
     # Переводим в нампи и нормализуем.
-    image_grX = np.array([image_grX])
-    prediction_array_grey = np.array(image_grX)/255
+    image_grX = np.array(image_grX)
+    # col.text(f'image_grX shape = {image_grX.shape}')
+    # prediction_array_grey = np.array(image_grX)/255
     # col.text(f'grX.shape={type(image_grX)}')
     # col.text(f'{image_grX}')
-    # predict = np.argmax(
-    #     model.predict([prediction_array_grey]), axis=-1)
+    predict = np.argmax(
+        model_grey.predict(aa), axis=-1)
     # Подготовка цветов классов для отрисовки предсказания
-    # prediction_array_grey = labels_to_rgb(predict[..., None])
+    prediction_array_grey = labels_to_grayscale(predict[..., None])
     return image_gr, prediction_array_grey
